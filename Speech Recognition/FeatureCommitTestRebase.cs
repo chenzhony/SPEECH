6f0cc92f7e3bf08c6ee5c9b9c1956337e7dbb427
chenzhony <czy89897@gmail.com>
2021-01-16 18:27:57 +0000

    Feature branch (#3)
    hahah

---
+++ Speech Recognition/FeatureCommitTestRebase.cs
@@ -0,0 +1,11 @@
+namespace DefaultNamespace
+{
+    public class FeatureCommitTestRebase
+    {
+        private var fristCommit = "first commit";
+        private var secondCommit = "second commit";
+        private var threeCommit = "three commit";
+        
+        private var fourCommit = "four commit";
+    }
+}
